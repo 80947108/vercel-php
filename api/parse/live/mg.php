4def747cda29a02f1f6a02c1b43f183b68e06655
80947108 <42614839+80947108@users.noreply.github.com>
2024-06-17 11:47:47 +0800

    Create mg.php

---
+++ api/parse/live/mg.php
@@ -0,0 +1,273 @@
+<?php
+
+$n = [
+
+    'cctv1' => 'wd_r2/cctv/cctv1hd/2500', //CCTV1综合HD
+
+    'cctv2' => 'wd_r2/cctv/cctv2hd/3000', //CCTV2财*HD
+
+    'cctv3' => 'wd_r2/ocn/cctv3hd/3000', //CCTV3综艺HD
+
+    'cctv4' => 'wd_r2/cctv/cctv4hd/3000', //CCTV4中文国际HD
+
+    'cctv4o' => 'migu/kailu/20200324/cctv4ouhd/57', //CCTV4中文国际-欧洲HD
+
+    'cctv4a' => 'migu/kailu/20200324/cctv4meihd/57', //CCTV4中文国际-美洲HD
+
+    'cctv5' => 'wd_r2/cctv/cctv5hdnew/2500', //CCTV5体育HD
+
+    'cctv5p' => 'wd_r2/cctv/cctv5plusnew/2500', //CCTV5+体育赛事HD
+
+    'cctv6' => 'wd_r2/ocn/cctv6hd/3000', //CCTV6电影HD
+
+    'cctv7' => 'migu/kailu/20200324/cctv7hd/57', //CCTV7国防军事HD
+
+    'cctv8' => 'wd_r2/ocn/cctv8hd/3000', //CCTV8电视剧HD
+
+    'cctv9' => 'migu/kailu/20200324/cctv9hd/57', //CCTV9纪录HD
+
+    'cctv10' => 'wd_r2/ocn/cctv10hd/3000', //CCTV10科教HD
+
+    'cctv11' => 'migu/kailu/cctv11hd/57/20200103', //CCTV11戏曲HD
+
+    'cctv12' => 'migu/kailu/20200324/cctv12hd/57', //CCTV12社会与法HD
+
+    'cctv13' => 'migu/kailu/cctv13/57/20220913', //CCTV13新闻
+
+    'cctv14' => 'wd_r2/ocn/cctv14hd/3000', //CCTV14少儿HD
+
+    'cctv15' => 'migu/kailu/20200324/cctv15hd/57', //CCTV15音乐HD
+
+    'cctv17' => 'migu/kailu/20200324/cctv17hd/57', //CCTV17农村农业HD
+
+    'lgs' => 'migu/kailu/cctvlaogushi/50/20231109', //种养新影-老故事
+
+    'fxzl' => 'wd_r3/cnr/faxianzhilv/2000', //种养新影-发现之旅
+
+    'zxs' => 'migu/kailu/20210415/cctvzhongxuesheng/51', //种养新影-中学生
+
+    'cgtn' => 'envivo_x/2018/SD/cctvnews/1000', //CGTN
+
+    'cgtnjl' => 'envivo_v/2018/SD/cctv/doc/1000', //CGTN纪录
+
+    'cgtna' => 'envivo_v/SD/cctv/alabo/1000', //CGTN阿拉伯语
+
+    'cgtnf' => 'envivo_v/2018/SD/cctv/fayu/1000', //CGTN法语
+
+    'cgtne' => 'envivo_v/2018/SD/cctv/xibanya/1000', //CGTN西班牙语
+
+    'cgtnr' => 'envivo_v/2018/SD/cctv/eyu/1000', //CGTN俄语
+
+    'chcjtyy' => 'wd_r3/cnr/chcjtyy/3000', //CHC家庭影院
+
+    'chcdzdy' => 'wd_r1/2018/gesee/chcdzdy/600', //CHC动作电影
+
+    'dfws' => 'wd_r4/dfl/dongfangwshd/3000', //东方卫视HD
+
+    'cqws' => 'envivo_x/2018/SD/chongqing/1000', //重庆卫视
+
+    'jlws' => 'envivo_v/2018/SD/jilin/1000', //吉林卫视
+
+    'lnws' => 'wd_r2/ocn/liaoningwshd/3000', //辽宁卫视HD
+
+    'nmws' => 'envivo_w/2018/SD/neimeng/1000', //内蒙古卫视
+
+    'nxws' => 'envivo_x/2018/SD/ningxia/1000', //宁夏卫视
+
+    'gsws' => 'envivo_v/2018/SD/gansu/1000', //甘肃卫视
+
+    'qhws' => 'envivo_w/2018/SD/qinghai/1000', //青海卫视
+
+    'sxws' => 'migu/kailu/shan3xiws/57/20220809', //陕西卫视
+
+    'sdws' => 'wd-shandonghd-1200', //山东卫视HD
+
+    'hnws' => 'wd-henanwssd-600', //河南卫视
+
+    'hubws' => 'wd_r2/2018/ocn/hubeiwshd/2000', //湖北卫视HD
+
+    'hunws' => 'wd-hunanhd-2500', //湖南卫视HD
+
+    'jxws' => 'migu/kailu/jxwshd/57/20190820', //江西卫视HD
+
+    'jsws' => 'wd_r2/cctv/jiangsuhd/2500', //江苏卫视HD
+
+    'gdws' => 'ws_w/gdws/gdws3000/3000', //广东卫视HD
+
+    'dwqws' => 'wd_r3/2018/nfmedia/nfws/1000', //大湾区卫视
+
+    'gxws' => 'wd-guangxiwssd-600', //广西卫视
+
+    'ynws' => 'envivo_x/2018/SD/yunnan/1000', //云南卫视
+
+    'gzws' => 'wd-guizhouwssd-600', //贵州卫视
+
+    'xjws' => 'wd-xinjiangwssd-600', //新疆卫视
+
+    'xzws' => 'envivo_x/2018/SD/xizang/1000', //西藏卫视
+
+    'hinws' => 'envivo_x/SD/lvyou/711', //海南卫视
+
+    'shdy' => 'wd_r4/peoplecn/shdiaoyu/1200', //四海钓鱼
+
+    'shxwzh' => 'wd_r4/dfl/xinwenzonghehd/3000', //上海新闻综合
+
+    'dycj' => 'wd_r1/2018/bestv/dycj/1000', //第一财*
+
+    'dfys' => 'wd_r4/dfl/dianshijuhd/3000', //东方影视
+
+    'shjs' => 'wd_r2/ocn/jspdhd/3000', //上海纪实人文
+
+    'shics' => 'wd_r4/dfl/waiyusd/600', //上海ICS
+
+    'fztd' => 'migu/kailu/fztdhd/57/20230411', //法治天地
+
+    'hxjc' => 'migu/kailu/hxjchd/57/20230411', //欢笑剧场
+
+    'ly' => 'migu/kailu/leyouhd/57/20230411', //乐游HD
+
+    'mlzq' => 'migu/kailu/mlzuhd/57/20230411', //魅力足球
+
+    'jbty' => 'migu/kailu/jinbaotyhd/57/20230411', //劲爆体育
+
+    'yxfy' => 'wd_r2/ocn/youxifengyunhd/1200', //游戏风云
+
+    'qcxj' => 'migu/kailu/qicaixjhd/51/20230411', //七彩戏剧
+
+    'lttv' => 'migu/kailu/linyaodst/51/20191206', //临洮电视台 甘肃
+
+    'sdjy' => 'envivo_v/2018/SD/shandongedu/1000', //SDETV山东教育
+
+    'jyjs' => 'wd_r2/cctv/jinyingjishi/1200', //金鹰纪实
+
+    'jscs' => 'wd_r2/jstv/jschengshi/600', //江苏城市
+
+    'jszy' => 'wd_r2/jstv/jszongyi/600', //江苏综艺
+
+    'jsxw' => 'wd_r2/jstv/jsgonggong/600', //江苏新闻
+
+    'jsjy' => 'wd_r2/jstv/jsjiaoyu/600', //江苏教育
+
+    'jsgj' => 'wd_r2/jstv/jsguoji/600', //江苏国际
+
+    'jsys' => 'wd_r2/jstv/jsyingshi/600', //江苏影视
+
+    'jsty' => 'wd_r2/jstv/jstiyu/600', //江苏体育休闲
+
+    'ymkt' => 'wd_r2/jstv/ymkt/600', //优漫卡通
+
+   'njxwzh' => 'migu/kailu/nanjingxwzh/50/20230719', //南京新闻综合
+
+    'njjk' => 'migu/kailu/nanjingkj/50/20230719', //南京教科
+
+    'njsb' => 'migu/kailu/nanjingsb/50/20230719', //南京十八
+
+    'haxwzh' => 'wd_r2/jstv/haxinwenzonghe/600', //淮安新闻综合
+
+    'szxwzh' => 'wd_r2/jstv/szxinwenzonghe/600', //苏州新闻综合
+
+    'tz1' => 'wd_r2/jstv/tzxinwenzonghe/600', //泰州新闻综合
+
+    'xzxwzh' => 'wd_r2/jstv/xzxinwenzonghe/600', //徐州新闻综合
+
+    'sqxwzh' => 'wd_r2/jstv/sqxinwenzonghe/600', //宿迁新闻综合
+
+    'lygxwzh' => 'wd_r2/jstv/lygxinwenzonghe/600', //连云港新闻综合
+
+    'gdys' => 'wd_r1/2018/nfmedia/nfys/1000', //广东影视
+
+    'jjkt' => 'wd_r1/2018/nfmedia/jjkt/1000', //嘉佳卡通
+
+    'cbajd' => 'migu/lunbo/cba/57/20230310', //CBA经典
+
+    'gdjys' => 'wd_r4/virtuallive/5101059662/150', //掼蛋精英赛
+
+    'gqdp' => 'wd_r3/cctv/gaoqingdapian/600', //高清大片
+
+    'hpjxss' => 'migu/lunbo/zonghesports/57/20230310', //和平精英赛事
+
+    'hslbt' => 'migu/virtuallive2/5900002413/57/20210706', //红色轮播台
+
+    'hyyt' => 'wd_r2/virtuallive/5102010211/75', //华语乐坛最强音
+
+    'jddh' => 'wd_r3/cctv/jingdiandonghua/600', //经典动画大集合
+
+    'jdxgdy' => 'wd_r2/virtuallive/5100180869/150', //经典深圳旁边电影
+
+    'jsdp' => 'wd-virtuallive5100002089-150', //军事迷必看大片
+
+    'ljcwhg' => 'wd_r2/virtuallive/5102010211/75', //历届春晚回顾
+
+    'mg24hty' => 'wd_r2/virtuallive/tysy201903/150', //咪咕24小时体育
+
+    'mglbt' => 'migu/lunbo/eufootball/57/20230310', /轮播台
+
+    'nbajd' => 'migu/lunbo/nba/57/20230310', //NBA经典
+
+    'ozzqfy' => 'migu/lunbo/eufootball/57/20230310', //欧洲足球风云
+
+    'qmpp' => 'migu/lunbo/pingpang/57/20230310', //全民乒乓
+
+    'rjlb' => 'wd_r3/cctv/rejulianbo/600', //热剧联播
+
+    'sszjd' => 'wd_r3/cctv/saishijingdian/600', //赛事最经典
+
+    'ttmlh' => 'wd_r3/cctv/titanminglan/600', //体坛名栏汇
+
+    'ufcgdjx' => 'migu/lunbo/ufc/57/20230310', //UFC格斗精选
+
+    'wdlsjd' => 'migu/lunbo/wudaliansai/57/20230310', //五大联赛经典
+
+    'xpfyt' => 'wd-virtuallive5100030975-150', //新片放映厅
+
+    'yxlmss' => 'migu/lunbo/quanmin/57/20230310', //英雄联盟赛事
+
+    'zjlxc' => 'wd_r3/virtuallive/5100170828/75', //周杰伦现场
+
+    'zqzyp' => 'wd_r3/cctv/zuiqiangzongyi/600', //最强综艺趴
+
+    'xmhd' => 'ws_x/2018/xiongmao/HD/2000', //熊猫频道01高清
+
+    'xm1' => 'envivo_x/2018/SD/xiongmao01/1000', //熊猫频道1
+
+    'xm2' => 'envivo_x/2018/SD/xiongmao02/1000', //熊猫频道2
+
+    'xm3' => 'envivo_x/2018/SD/xiongmao03/1000', //熊猫频道3
+
+    'xm4' => 'envivo_x/2018/SD/xiongmao04/1000', //熊猫频道4
+
+    'xm5' => 'envivo_x/2018/SD/xiongmao05/1000', //熊猫频道5
+
+    'xm6' => 'envivo_x/2018/SD/xiongmao06/1000', //熊猫频道6
+
+    'xm7' => 'envivo_x/2018/SD/xiongmao07/1000', //熊猫频道7
+
+    'xm8' => 'envivo_x/2018/SD/xiongmao08/1000', //熊猫频道8
+
+    'xm9' => 'envivo_x/2018/SD/xiongmao09/1000', //熊猫频道9
+
+    'xm10' => 'envivo_x/2018/SD/xiongmao10/1000', //熊猫频道10
+
+];
+
+
+$id = $_GET['id'];
+
+
+if (array_key_exists($id, $n)) {
+
+
+    $url = "http://lr1.aikan.miguvideo.com/".$n[$id]."/index.m3u8?msisdn=&encrypt=";
+
+    header('Location: '.$url);
+
+    exit;
+
+} else {
+
+    echo "Invalid id parameter";
+
+}
+
+
+?>
