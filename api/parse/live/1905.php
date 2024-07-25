569c6c2e58437f1b49e3ecaee8523798ce5a5aeb
80947108 <42614839+80947108@users.noreply.github.com>
2024-07-25 19:06:55 +0800

    Update 1905.php

--- api/parse/live/1905.php
+++ api/parse/live/1905.php
@@ -1,53 +1,26 @@
 <?php
+//https://www.1905.com/cctv6/live/
 error_reporting(0);
-$id = $_GET["id"] ?? "cctv6";
+$id = $_GET['id']??'cctv6';
 $n = [
-        "cctv6" => "LIVEOCTI36HXJXB9U",
-        "1905a" => "LIVENCOI8M4RGOOJ9",
-        "1905b" => "LIVE8J4LTCXPI7QJ5",
-];
-$ts = time();
-$salt = "2bcc2c6ab75dac016d20181bfcd1ee0697c78018";
-$playerId = substr($ts, 6) . substr(rand(), -3) . substr(rand(), -8);
-$uuid = strtolower(
-        sprintf(
-                "%04X%04X-%04X-%04X-%04X-%04X%04X%04X",
-                mt_rand(0, 65535),
-                mt_rand(0, 65535),
-                mt_rand(0, 65535),
-                mt_rand(16384, 20479),
-                mt_rand(32768, 49151),
-                mt_rand(0, 65535),
-                mt_rand(0, 65535),
-                mt_rand(0, 65535)
-        )
-);
-$params = [
-        "cid" => 999999,
-        "expiretime" => $ts + 600,
-        "nonce" => $ts,
-        "page" => "https://www.1905.com/",
-        "playerid" => $playerId,
-        "streamname" => $n[$id],
-        "uuid" => $uuid,
-];
-$sign = sha1(http_build_query($params) . "." . $salt);
-$params["appid"] = "GEalPdWA";
-$post = json_encode($params);
-$url = "https://profile.m1905.com/mvod/liveinfo.php";
-$headers = ["Authorization: " . $sign, "Content-Type: application/json"];
-$ch = curl_init($url);
+    'cctv6' => ['LIVENLPG8RMKR5TW6','8610274828f804c4e10e39b01a496357b852a326'],//CCTV6电影频道
+    '1905a' => ['LIVENCOI8M4RGOOJ9','42eb78debea1e7525597384f64eec95b26e3e9f8'],//1905国内电影
+    '1905b' => ['LIVE8J4LTCXPI7QJ5','a7a1d67d98944b7880b3593d3ea677727656952c'],//1905国外电影
+    ];
+
+$post = '{"cid":999999,"expiretime":"2000000600","nonce":"2000000000","page":"https://www.1905.com","playerid":"12345678","streamname":"'.$n[$id][0].'","uuid":"1","appid":"GEalPdWA"}';
+$ch = curl_init('https://profile.m1905.com/mvod/liveinfo.php');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_POST,1);
+curl_setopt($ch, CURLOPT_HTTPHEADER,['Authorization:'.$n[$id][1]]);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
 $data = curl_exec($ch);
 curl_close($ch);
 $json = json_decode($data);
-$playurl =
-        "https://hlslive.1905.com/live/$n[$id]/index.m3u8" .
-        $json->data->sign->hd->sign;
+$playurl = "http://hlslive.1905.com/live/{$n[$id][0]}/index.m3u8".$json->data->sign->hd->sign;
 header("Content-Type: application/vnd.apple.mpegURL");
-header("location:" . $playurl);
+header('location:'.$playurl);
+//echo $playurl;
+?>
